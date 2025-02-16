5590784bc3336673c35f9ef8f9afda4777b5ff94
Jose Luis Rosello Ferrer <jl1284@gmail.com>
2025-02-16 15:51:56 +0100

    load script changes

--- load_data_to_bq.py
+++ load_data_to_bq.py
@@ -4,33 +4,26 @@ import os
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "keys/creds.json"
 
-def load_weather_stations_to_bq():
-    bucket_url = "gs://taxis-bucket-448121-i4/weather_stations"
-
+def load_data_to_bq(bucket_url, pipeline_name, table_name):
+    print(f"Starting load for {table_name}...")
     pipeline = dlt.pipeline(
-        pipeline_name='load_bq_weather_stations',
-        destination='bigquery', 
-        dataset_name='dlt',
+        pipeline_name=pipeline_name,
+        destination="bigquery", 
+        dataset_name="dlt",
     )
 
     parquet_reader = readers(bucket_url, file_glob="**/*.parquet").read_parquet()
-    load_info = pipeline.run(parquet_reader.with_name("weather_stations"),write_disposition="replace")
+    load_info = pipeline.run(parquet_reader.with_name(table_name), write_disposition="replace")
     print(load_info)
     print(pipeline.last_trace.last_normalize_info)
+
+def load_weather_stations_to_bq():
+    bucket_url = "gs://taxis-bucket-448121-i4/weather_stations"
+    load_data_to_bq(bucket_url, "load_bq_weather_stations", "weather_stations")
 
 def load_climatic_values_to_bq():
     bucket_url = "gs://taxis-bucket-448121-i4/climatic_values"
-
-    pipeline = dlt.pipeline(
-        pipeline_name='load_bq_climatic_values',
-        destination='bigquery', 
-        dataset_name='dlt',
-    )
-
-    parquet_reader = readers(bucket_url, file_glob="**/*.parquet").read_parquet()
-    load_info = pipeline.run(parquet_reader.with_name("climatic_values"),write_disposition="replace")
-    print(load_info)
-    print(pipeline.last_trace.last_normalize_info)
+    load_data_to_bq(bucket_url, "load_bq_climatic_values", "climatic_values")
 
 # Call the functions
 load_weather_stations_to_bq()
