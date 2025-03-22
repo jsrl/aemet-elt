318a96bf29537d637bd12e91b787079807e79b26
Jose Luis Rosello <nagast0rm1284@gmail.com>
2025-03-22 11:11:10 +0000

    fix Beaufort scale.

--- dbt/macros/get_velmedia_description.sql
+++ dbt/macros/get_velmedia_description.sql
@@ -1,20 +1,21 @@
 {#
-    This macro returns the description of the air velmedia
+    This macro returns the description of the air velmedia according to the Beaufort scale.
 #}
 
 {% macro categorize_velmedia(velmedia) %}
     case
-        when {{ velmedia }} between 0 and 5.4 then 'Calm'
-        when {{ velmedia }} between 5.5 and 11.9 then 'Light Breeze'
-        when {{ velmedia }} between 12.0 and 19.4 then 'Gentle Breeze'
-        when {{ velmedia }} between 19.5 and 28.4 then 'Moderate Breeze'
-        when {{ velmedia }} between 28.5 and 38.5 then 'Fresh Breeze'
-        when {{ velmedia }} between 38.6 and 49.7 then 'Strong Breeze'
-        when {{ velmedia }} between 49.8 and 61.6 then 'Near Gale'
-        when {{ velmedia }} between 61.7 and 74.5 then 'Gale'
-        when {{ velmedia }} between 74.6 and 87.9 then 'Strong Gale'
-        when {{ velmedia }} between 88.0 and 102.2 then 'Storm'
-        when {{ velmedia }} between 102.3 and 117.4 then 'Violent Storm'
+        when {{ velmedia }} < 0.3 then 'Calm'
+        when {{ velmedia }} between 0.3 and 1.5 then 'Light Air'
+        when {{ velmedia }} between 1.6 and 3.3 then 'Light Breeze'
+        when {{ velmedia }} between 3.4 and 5.4 then 'Gentle Breeze'
+        when {{ velmedia }} between 5.5 and 7.9 then 'Moderate Breeze'
+        when {{ velmedia }} between 8.0 and 10.7 then 'Fresh Breeze'
+        when {{ velmedia }} between 10.8 and 13.8 then 'Strong Breeze'
+        when {{ velmedia }} between 13.9 and 17.1 then 'Near Gale'
+        when {{ velmedia }} between 17.2 and 20.7 then 'Gale'
+        when {{ velmedia }} between 20.8 and 24.4 then 'Strong Gale'
+        when {{ velmedia }} between 24.5 and 28.4 then 'Storm'
+        when {{ velmedia }} between 28.5 and 32.6 then 'Violent Storm'
         else 'Hurricane-force'
     end
 {% endmacro %}
--- dbt/models/staging/stg_weather_stations.sql
+++ dbt/models/staging/stg_weather_stations.sql
@@ -4,15 +4,14 @@ with weather_stations as (select * from {{ source("staging", "weather_stations")
 
 select
     {{ dbt_utils.generate_surrogate_key(["indicativo", "nombre"]) }} as record_id,
+    nombre,
+    indicativo
+    provincia,
+    {{ categorize_provincia("provincia") }} as autonomous_community
     --_dlt_id,
     --longitud,
     --indsinop,
     --_dlt_load_id,
-    nombre,
-    indicativo,
     --latitud,
-    --altitud,
-    provincia,
-    {{ categorize_provincia("provincia") }} as autonomous_community,
-
+    --altitud,    
 from weather_stations
