9e6578ec8c7f20c75e71bddfefe55363ca041672
jsrl <jl1284@gmail.com>
2025-03-22 17:06:42 +0100

    added cluster param

--- dlt/load_data_to_bq.py
+++ dlt/load_data_to_bq.py
@@ -5,7 +5,7 @@ from dlt.destinations.adapters import bigquery_adapter
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "keys/creds.json"
 
-def load_data_to_bq(bucket_url, pipeline_name, table_name, partition_field=None):
+def load_data_to_bq(bucket_url, pipeline_name, table_name, partition_field=None, cluster_field=None):
     print(f"Starting load for {table_name}...")
     pipeline = dlt.pipeline(
         pipeline_name=pipeline_name,
@@ -14,18 +14,18 @@ def load_data_to_bq(bucket_url, pipeline_name, table_name, partition_field=None)
     )
     
     parquet_reader = readers(bucket_url, file_glob="**/*.parquet").read_parquet()
-    load_info = pipeline.run(bigquery_adapter(parquet_reader.with_name(table_name), autodetect_schema=False, partition=partition_field), 
+    load_info = pipeline.run(bigquery_adapter(parquet_reader.with_name(table_name), autodetect_schema=False, partition=partition_field, cluster=cluster_field), 
                              write_disposition="replace")
     print(load_info)
     print(pipeline.last_trace.last_normalize_info)
 
 def load_weather_stations_to_bq():
     bucket_url = "gs://aemet-weather-data-bucket/weather_stations"
-    load_data_to_bq(bucket_url, "load_bq_weather_stations", "weather_stations")
+    load_data_to_bq(bucket_url, "load_bq_weather_stations", "weather_stations", cluster_field="provincia")
 
 def load_climatic_values_to_bq():
     bucket_url = "gs://aemet-weather-data-bucket/climatic_values"
-    load_data_to_bq(bucket_url, "load_bq_climatic_values", "climatic_values", partition_field="ano")
+    load_data_to_bq(bucket_url, "load_bq_climatic_values", "climatic_values", partition_field="ano", cluster_field="indicativo")
 
 load_weather_stations_to_bq()
 load_climatic_values_to_bq()
