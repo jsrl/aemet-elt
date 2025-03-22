4621a5064a367a75dc0e6beb91c8f37c752bcd0c
Jose Luis Rosello <nagast0rm1284@gmail.com>
2025-03-22 10:58:29 +0000

    Removing unused fields from staging

--- dbt/models/staging/stg_weather_stations.sql
+++ dbt/models/staging/stg_weather_stations.sql
@@ -4,14 +4,14 @@ with weather_stations as (select * from {{ source("staging", "weather_stations")
 
 select
     {{ dbt_utils.generate_surrogate_key(["indicativo", "nombre"]) }} as record_id,
-    _dlt_id,
-    longitud,
-    indsinop,
-    _dlt_load_id,
+    --_dlt_id,
+    --longitud,
+    --indsinop,
+    --_dlt_load_id,
     nombre,
     indicativo,
-    latitud,
-    altitud,
+    --latitud,
+    --altitud,
     provincia,
     {{ categorize_provincia("provincia") }} as autonomous_community,
 
